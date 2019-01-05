282196c8d5cf6b5d76ef2881e637cee45c2ca841
andrei-kolesnik <andrei.kolesnik@gmail.com>
2019-01-05 13:33:50 -0800

    Network Delay Time

--- 743.Network-Delay-Time.STL/743.Network-Delay-Time.STL.cpp
+++ 743.Network-Delay-Time.STL/743.Network-Delay-Time.STL.cpp
@@ -32,7 +32,7 @@ size_t minDistance(vector<int>& dist, vector<bool>& sptSet, int N) {
 
 int networkDelayTime(vector<vector<int>>& times, int N, int K) {
 	if (!times.size()) return -1;
-	vector<vector<int>> matrix = edges2matrix(times, N);
+	vector<vector<int>> matrix = edges2matrix(times, N);	
 	vector<int> dist(N + 1, INT32_MAX);
 	vector<bool> sptSet(N + 1, false);
 	dist[K] = 0;
@@ -41,16 +41,49 @@ int networkDelayTime(vector<vector<int>>& times, int N, int K) {
 		size_t md = minDistance(dist, sptSet, N);
 		sptSet[md] = true;
 		for (int j = 1; j <= N; j++)
-			if (!sptSet[j] && matrix[md][j] >= 0 &&
-				dist[md] != INT32_MAX && dist[md] + matrix[md][j] < dist[j])
+			if (!sptSet[j] && // not yet visited
+				matrix[md][j] >= 0 && // has a path from md to j
+				dist[md] != INT32_MAX && // already calculated
+				dist[md] + matrix[md][j] < dist[j]) // shoorted than calculated before
+			{
 				dist[j] = dist[md] + matrix[md][j];
+			}
 	}
-	for (auto d : dist) cout << d << " ";
+	//## for (auto d : dist) cout << d << " "; //debug print
 	int result = *max_element(dist.begin() + 1, dist.end());
-	if (result == INT32_MAX) result = -1;
-	return result;
+	return result == INT32_MAX ? -1 : result;
 }
 
+/* Version 2: optimized with with heap, but does not pass all the tests
+int networkDelayTime2(vector<vector<int>>& times, int N, int K) {
+	if (!times.size()) return -1;
+	vector<vector<int>> matrix = edges2matrix(times, N);
+	vector<int> dist(N + 1, INT32_MAX);
+	priority_queue<pair<int, int>, vector<pair<int, int>>> distq;
+	vector<bool> sptSet(N + 1, false);
+	dist[K] = 0;
+	distq.push(make_pair(0, K));
+	while (!distq.empty())
+	{
+		size_t md = distq.top().second; distq.pop();
+		if (sptSet[md]) continue;
+		sptSet[md] = true;
+		for (int j = 1; j <= N; j++)
+			//if (!sptSet[j] && matrix[md][j] >= 0 &&
+			//	dist[md] != INT32_MAX && dist[md] + matrix[md][j] < dist[j])
+			if (matrix[md][j] >= 0 && 
+				dist[md] != INT32_MAX &&
+				dist[md] + matrix[md][j] < dist[j])
+			{
+				dist[j] = dist[md] + matrix[md][j];
+				distq.push(make_pair(dist[j], j));
+			}
+	}
+	//## for (auto d : dist) cout << d << " "; //debug print
+	int result = *max_element(dist.begin() + 1, dist.end());
+	return result == INT32_MAX ? -1 : result;
+}
+*/
 void check(int e[][3], int size, int N, int K, int expected) {
 	vector<vector<int>> edges;
 	for (size_t i = 0; i < size; i++)
@@ -95,6 +128,30 @@ int main()
 		{ 2,3,59 }
 	};
 	check(es2, 20, 5, 5, 31);
+
+	int es3[20][3] = { 
+		{ 4,2,76 }, 
+		{ 1,3,79 }, 
+		{ 3,1,81 }, 
+		{ 4,3,30 },	
+		{ 2,1,47 }, 
+		{ 1,5,61 }, 
+		{ 1,4,99 }, 
+		{ 3,4,68 }, 
+		{ 3,5,46 }, 
+		{ 4,1, 6 }, 
+		{ 5,4, 7 },	
+		{ 5,3,44 }, 
+		{ 4,5,19 }, 
+		{ 2,3,13 }, 
+		{ 3,2,18 }, 
+		{ 1,2, 0 }, 
+		{ 5,1,25 }, 
+		{ 2,5,58 }, 
+		{ 2,4,77 }, 
+		{ 5,2,74 }
+	};
+	check(es3, 20, 5, 3, 59);
 
 	system("pause");
 	return 0;
