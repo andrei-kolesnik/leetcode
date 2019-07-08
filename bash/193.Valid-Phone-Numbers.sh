f299b7cc2acc33117e618ccfad8b3d872d341233
Andrei Kolesnik <andrei@pinger.com>
2019-07-07 19:25:44 -0700

    Valid Phone Numbers

---
+++ bash/193.Valid-Phone-Numbers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# LettCode 193. Valid Phone Numbers
+# https://leetcode.com/problems/valid-phone-numbers/
+
+# Version 1: awk
+awk '/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/' file.txt
