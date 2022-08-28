ac1e5022679b5a4685b21ebaaf9cc27d1b53bc86
Andrei <andrei.kolesnik@gmail.com>
2022-08-28 11:21:57 -0700

    Add Two Numbers (python)

--- Python/002.Add-Two-Numbers.py
+++ Python/002.Add-Two-Numbers.py
@@ -1,8 +1,10 @@
-# LeetCode #2: Add Two Numbers
-# https://leetcode.com/problems/add-two-numbers/
-# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-# Output: 7 -> 0 -> 8
-# Explanation: 342 + 465 = 807.
+"""
+LeetCode #2: Add Two Numbers
+https://leetcode.com/problems/add-two-numbers/
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
 
 from typing import Optional, Union
 
